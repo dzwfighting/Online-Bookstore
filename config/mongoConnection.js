a1e027b7b8f98fe39a8be6876b197ded64f38f2d
Zhipeng yin <zhip_y@yeah.net>
2022-04-20 20:07:50 -0400

    The project structure is initialized

---
+++ config/mongoConnection.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb').MongoClient;
+const settings = {
+  "mongoConfig": {
+    "serverUrl": "mongodb://localhost:27017/",
+    "database": "Zhipeng_Yin_lab6"
+  }
+};
+
+let _connection = undefined;
+let _db = undefined;
+
+module.exports = {
+  connectToDb: async () => {
+    if (!_connection) {
+      _connection = await MongoClient.connect(settings.mongoConfig.serverUrl);
+      _db = await _connection.db(settings.mongoConfig.database);
+    }
+
+    return _db;
+  },
+  closeConnection: () => {
+    _connection.close();
+  }
+};
