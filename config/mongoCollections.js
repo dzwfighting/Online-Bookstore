4bdc63dd762be98b127fa2d86cfe057868feb7e8
dzwfighting <yiwen7943@gmail.com>
2022-04-23 01:25:02 -0400

    add comments

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -17,5 +17,6 @@ const getCollectionFn = (collection) => {
 
 /* Now, you can list your collections here: */
 module.exports = {
+  posts: getCollectionFn("posts"),
   comments: getCollectionFn("comments")
 };
