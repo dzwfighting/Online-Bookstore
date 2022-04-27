01448572b761b1f1fb5483c01355aacbda8cd62c
dzwfighting <yiwen7943@gmail.com>
2022-04-27 16:53:10 -0400

    renew routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
+const userRoutes = require("./users");
+const postRoutes = require("./posts");
+const commentRoutes = require("./comments");
+const reportRoutes = require("./reports");
 const main = require("./main");
 const constructorMethod = (app) => {
+    app.use("/users", userRoutes);
+    app.use("/posts", postRoutes);
+    app.use("/comments", commentRoutes);
+    app.use("/reports", reportRoutes);
     app.use("/home", main);
 
-
-
-
-
-
+    app.get('/', (req, res) => {
+        res.redirect('http://localhost:3000/homePage');
+    });
 
 
     app.use("*", (req, res) => {
