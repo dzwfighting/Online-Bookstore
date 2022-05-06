aeeb7d462492c32c7065d3aafd9d506acb543206
dzwfighting <yiwen7943@gmail.com>
2022-05-06 00:23:18 -0400

    add check if admin in routes/users

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,41 @@ module.exports = router;
 
 // router.get("/", async (req, res) => {
 //         if (req.session.user) {
-//             return res.redirect("/homepage");
+//             return res.redirect("/books");
 //         } else {
 //             return res.render("users/login", {title: "LogIn Page"});
 //         }
-//     });
+// });
+
+
+// router.post('/', async (req, res) => {
+//     const loginForm = req.body
+//     if (!loginForm.username
+//         || loginForm.username.includes(' ')
+//         || !loginForm.username.match(/^[0-9a-zA-Z]+$/)
+//         || loginForm.username.length < 4){
+
+//         throw "You should input a valid userName"
+//     }
+//     if (!loginForm.password || loginForm.password.includes(' ') || loginForm.password.length<6){
+//         throw "You should input a valid password"
+//     }
+
+    
+//     if(await adminDate.checkManager(loginForm.username, loginForm.password)){
+//         req.session.user = { username: loginForm.username, accountType: 'admin' };
+//         res.redirect('/books')
+//         return
+//     }
+    
+//     return res.status(400).render("users/login",{
+//         hasErrors:true,
+//         error : e,
+//     })
+
+// });
+
+
 router.get("/signup", async (req, res) => {
         console.log("sign up")
 
@@ -55,7 +85,7 @@ router.get("/login", async (req, res) => {
         }else {
             return  res.render("users/login", {
                 title:"LogIn Page"
-            });
+            }); 
         }
     });
 
@@ -68,6 +98,13 @@ router.post("/login",async (req,res)=>{
                 userId:user.userId,
                 email:user.email
             };
+
+            //to check admin login
+            if(await adminDate.checkManager(loginForm.username, loginForm.password)){
+            req.session.user = { username: loginForm.username, accountType: 'admin' };
+            res.redirect('/books')
+            return
+        }
 
             return res.status(200).redirect("/users/profile")
         }catch (e){
