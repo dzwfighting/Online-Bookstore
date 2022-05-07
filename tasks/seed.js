62670bd483a3badd566de4e730444c112c259c5f
dzwfighting <yiwen7943@gmail.com>
2022-05-07 04:20:12 -0400

    add a admin to login

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,6 +1,3 @@
-const adminSeed = require('./seedAdmin');
-// const userSeed = require('./seedUsers');
-// const reviewSeed = require('./seedReviews');
 const connection = require('../config/mongoConnection');
 const adminData = require("../data/admin");
 
@@ -11,12 +8,13 @@ async function main() {
   const db = await connection();
   await db.dropDatabase();
 
-  await adminData.createAdmin("admin1", "dzwwyw7943@gmail.com", "admin1");
+  let a = await adminData.createAdmin("admin1", "admin1");
+  let a_userid = a._id.toHexString();
   console.log("admin1 add success");
-  await adminData.createAdmin("admin2", "fight@gmail.com", "admin2");
-  console.log("admin2 add success")
-  await adminData.createAdmin("admin3", "successful@gmail.com", "admin3");
-  console.log("admin3 add success")
+  // await adminData.createAdmin("admin2", "fight@gmail.com", "admin2");
+  // console.log("admin2 add success")
+  // await adminData.createAdmin("admin3", "successful@gmail.com", "admin3");
+  // console.log("admin3 add success")
   
   
   console.log("Adminstrations have been created...");
