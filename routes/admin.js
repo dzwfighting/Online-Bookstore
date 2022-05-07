489a2562760a8225a6d19dc04ed0fc12d8e89f55
dzwfighting <yiwen7943@gmail.com>
2022-05-07 04:24:25 -0400

    renew admin

--- routes/admin.js
+++ routes/admin.js
@@ -7,39 +7,64 @@ const bcrypt = require('bcrypt');
 const saltRounds = 16;
 const xss = require('xss');
 
-router.get("/login", async (req, res) => {
-        console.log("log in")
-        console.log(req.session)
-        if (req.session.user) {
-            return res.redirect("/adminPage");
-        }else {
-            return  res.render("admin/login", {
-                title:"LogIn Page"
-            }); 
-        }
-    });
 
-router.post("/login",async (req,res)=>{
-    if(req.body.username ==  '' || req.body.password ==  '' ) throw 'Please fill all fields';
-    if (await adminData.isManager(req.body.username)) {
-        req.session.user = { username: req.body.username, ifAdmin: true };
-        res.redirect('/admin/profile')
-        return
-    } else {
-         res.status(500).render('/admin/login', {title: "Login", error: 'The Username you input is not admin'})
-        return
-    }   
+router.get('/login', async (req, res) =>{
+    if(req.session.adminId){
+        return res.redirect('/homepage');
+    }
+    else {
+        res.render('admin/login', {
+            title: 'admin Login',
+            partial: 'login-script'
+        });
+    }
 });
 
-router.get('/profile', async (req, res) => {
-    let saveduser = {}
-    saveduser = req.session.user
-    username = saveduser.username
-    let user = await usersData.findUserByName(username)
-    res.status(200).render('admin/adminPage', {
-        user: user
+
+router.post('/login', async (req, res) =>{
+    if(req.session.adminId){
+        return res.redirect('/homepage');
+    }
+    else {
+        let {username, password} = req.body;
+        const allAdmin = await adminData.getAllAdmin();
+        for(let x of allAdmin){
+            if(username === x.username){
+                if(await bcrypt.compare(password, x.password)){
+                    req.session.adminId = x._id.toHexString();
+                    res.redirect('/admin/profile');
+                }
+                break;
+            }
+        }
+        res.status(401).render('admin/login', {message: "Invalid username or password", partial: 'login-script'});
+    }
+});
+
+
+
+    
+router.get('/profile',async(req,res)=>{
+    if(!req.session.adminId){
+        res.redirect('../admin/login')
+    }
+    let userInformation = await adminData.getAdminById(req.session.adminId);
+    res.status(200).render('admin/adminPage',{
+        userInformation,
+        partial: 'admin-script',
+        admin: true
     })
 });
+
+router.get('/logout', async (req, res) => {
+    if (!req.session.adminId) {
+        return res.redirect('/homepage');
+    }else {
+        req.session.destroy();
+        return res.redirect('/homepage');
+    }
+});
+
 
 module.exports = router;
 
